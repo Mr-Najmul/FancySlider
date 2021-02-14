cb2c6e71f9eff0303a9afb921c31fb68ee96fbec
Mr-Najmul <nazmul140095@gmail.com>
2021-02-14 23:49:30 +0600

    final

--- app.js
+++ app.js
@@ -1,159 +1,156 @@
 const imagesArea = document.querySelector('.images');
-const errorMassage = document.getElementById('error-massage');
+const errorMessage = document.getElementById('error-message');
 const gallery = document.querySelector('.gallery');
 const galleryHeader = document.querySelector('.gallery-header');
-const search = document.getElementById('search');
+const searchInput = document.getElementById('search');
 const searchBtn = document.getElementById('search-btn');
 const sliderBtn = document.getElementById('create-slider');
 const sliderContainer = document.getElementById('sliders');
-// selected image 
+
 let sliders = [];
-
-
 //API KEY
-
 const KEY = '15674931-a9d714b6e9d654524df198e00&q';
 
-// show images 
+
 const showImages = (images) => {
-  errorMassage.classList.add('d-none');
-  imagesArea.classList.remove('d-none');
-  gallery.innerHTML = '';
-  // show gallery title
-  galleryHeader.style.display = 'flex';
-  images.forEach(image => {
-    let div = document.createElement('div');
-    div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
-    div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
-    gallery.appendChild(div)
-  })
-  loadingBar();
-}
+  if (images.length === 0 || searchInput.value === '') {
+    imagesArea.style.display = 'none';
+    errorMessage.style.display = 'block';
+
+    if (searchInput.value === '') {
+      errorMessage.innerHTML = `
+      <h2 class="text-center text-danger mt-4">Sorry please try again.</h2>`;
+    } else {
+      errorMessage.innerHTML = `
+      <h2 class="text-center text-danger mt-4">This item does not exist, Sorry please try again.</h2>`;
+    }
+  } else {
+    errorMessage.style.display = 'none';
+    imagesArea.style.display = 'block';
+    gallery.innerHTML = '';
+ 
+    galleryHeader.style.display = 'flex';
+    images.forEach((image) => {
+      let div = document.createElement('div');
+      div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
+      div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
+      gallery.appendChild(div);
+    });
+    searchInput.value = '';
+  }
+  document.getElementById('duration').value = '';
+  showSpinner(false);
+};
+
+
+searchInput.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    searchBtn.click();
+  }
+});
 
 const getImages = (query) => {
-  fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=false`)
-    .then(response => response.json())
-    .then(data => {
-      if(data.hits.length > 0){
-         showImages(data.hits);
-      }else{
-        notFind();
-      }
-    }) 
-    .catch(error => notFind())
-}
-
+  showSpinner(true);
+  fetch(
+    `https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`
+  )
+    .then((response) => response.json())
+    .then((data) => showImages(data.hits))
+    .catch((err) => console.log(err));
+};
 
 let slideIndex = 0;
 const selectItem = (event, img) => {
   let element = event.target;
-  element.classList.add('added');
- 
+  element.classList.toggle('added');
+
   let item = sliders.indexOf(img);
   if (item === -1) {
     sliders.push(img);
   } else {
     sliders.splice(item, 1);
-    element.classList.remove('added');
   }
-}
-
-
-var timer
+};
+var timer;
 const createSlider = () => {
-  // check slider image length
   if (sliders.length < 2) {
-    alert('Select at least 2 image.')
+    alert('Select at least 2 image.');
     return;
   }
-  // crate slider previous next area
+
   sliderContainer.innerHTML = '';
   const prevNext = document.createElement('div');
-  prevNext.className = "prev-next d-flex w-100 justify-content-between align-items-center";
+  prevNext.className =
+    'prev-next d-flex w-100 justify-content-between align-items-center';
   prevNext.innerHTML = ` 
   <span class="prev" onclick="changeItem(-1)"><i class="fas fa-chevron-left"></i></span>
   <span class="next" onclick="changeItem(1)"><i class="fas fa-chevron-right"></i></span>
   `;
 
-  sliderContainer.appendChild(prevNext)
+  sliderContainer.appendChild(prevNext);
   document.querySelector('.main').style.display = 'block';
-  // hide image aria
-  imagesArea.classList.add('d-none');
-  const durationInput = document.getElementById('duration').value || 1000;
-  const duration = durationInput < 1 ? 1000 : durationInput * 1000;  // const duration
-  sliders.forEach(slide => {
-    let item = document.createElement('div')
-    item.className = "slider-item";
+
+  imagesArea.style.display = 'none';
+  let duration = document.getElementById('duration').value || 1000;
+  if (duration >= 1000) {
+    duration = duration;
+  } else {
+    duration = 1000;
+  }
+  sliders.forEach((slide) => {
+    let item = document.createElement('div');
+    item.className = 'slider-item';
     item.innerHTML = `<img class="w-100"
-    src="${slide}"
-    alt="">`;
-    sliderContainer.appendChild(item)
-  })
-  changeSlide(0)
+      src="${slide}"
+      alt="">`;
+    sliderContainer.appendChild(item);
+  });
+  changeSlide(0);
   timer = setInterval(function () {
     slideIndex++;
     changeSlide(slideIndex);
   }, duration);
-}
+};
 
-// change slider index 
-const changeItem = index => {
-  changeSlide(slideIndex += index);
-}
+const changeItem = (index) => {
+  changeSlide((slideIndex += index));
+};
 
-// change slide item
 const changeSlide = (index) => {
-
   const items = document.querySelectorAll('.slider-item');
   if (index < 0) {
-    slideIndex = items.length - 1
+    slideIndex = items.length - 1;
     index = slideIndex;
-  };
+  }
 
   if (index >= items.length) {
     index = 0;
     slideIndex = 0;
   }
 
-  items.forEach(item => {
-    item.style.display = "none"
-  })
+  items.forEach((item) => {
+    item.style.display = 'none';
+  });
 
-  items[index].style.display = "block"
-}
+  items[index].style.display = 'block';
+};
 
 searchBtn.addEventListener('click', function () {
-  searchAlbum();
-})
-search.addEventListener('keypress', function (event) {
-  if(event.key === 'Enter'){
-    searchAlbum();
-  }
-})
-
-function searchAlbum(){
   document.querySelector('.main').style.display = 'none';
   clearInterval(timer);
   const search = document.getElementById('search');
-  loadingBar();
-  getImages(search.value)
+  getImages(search.value);
   sliders.length = 0;
-}
+});
 
 sliderBtn.addEventListener('click', function () {
-  createSlider()
-})
+  createSlider();
+});
 
-
-// loading spinner added  
-function loadingBar(){
-  const loading = document.getElementById('loading-spinner');
-  loading.classList.toggle('d-none');
-}
-
- 
-function notFind(){
-  loadingBar();
-  errorMassage.classList.remove('d-none');
-  imagesArea.classList.add('d-none')
-}
+//Show Spinner
+const showSpinner = (show) => {
+  const spinner = document.getElementById('lodding-spinner');
+  show ? spinner.classList.remove('d-none') : spinner.classList.add('d-none');
+  errorMessage.classList.toggle('d-none');
+  imagesArea.classList.toggle('d-none');
+};
